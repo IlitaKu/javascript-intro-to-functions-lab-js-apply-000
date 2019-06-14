7d8da1eb5dd5f7c4be928471f9337a37f2c5e107
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-14 20:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log() (string.toUpperCase())
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log() (string.toLowerCase())
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
